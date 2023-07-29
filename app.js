9d874a65fa29b0324d7dd5eadd17cb6f8f6fc2d3
Dipak <paudelsantosh508@gmail.com>
2023-07-29 14:00:20 +1000

    small change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/frontend/build')))
 app.get('/names',(req,res)=>{
-    res.send('sulav adhikari');
+    res.send('dipak paudel');
 });
 
 app.listen(5000,()=>{
